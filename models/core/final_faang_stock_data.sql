89f8a47f2ff387d9bd7e79056d026efb7b29dce5
Enoch Kim <enochiankim@gmail.com>
2023-04-06 00:08:49 +0000

    Changes

--- models/core/final_faang_stock_data.sql
+++ models/core/final_faang_stock_data.sql
@@ -1,4 +1,18 @@
 {{ config(materialized='table') }}
 
 
-with faang_stock_data_main(select * from {{ref("faang_stock_data")}})
+with 
+stock_data_main as (select * from {{ref("faang_stock_data")}}),
+stock_data_symbol as (select * from {{ref("faang_stock_symbol")}})
+
+select 
+Stock_Symbol,
+Company,
+Date,
+Open,
+High, 
+Low,
+Close
+Adj_Close,
+Volume
+from stock_data_main m join stock_data_symbol s on m.file_name = s.company
